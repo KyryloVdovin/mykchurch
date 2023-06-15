079f7a4288444dc8849f7d67fe589974100efafd
Kyrylo Vdovin <Vdovin.krll2000@gmail.com>
2023-06-15 23:00:04 +0300

    - added header component and set styles

---
+++ src/components/header/header.jsx
@@ -0,0 +1,14 @@
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <p className='site-title'>
+                    <a href='https://nikchurch.wordpress.com/'>Церковь Иисуса Христа</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
